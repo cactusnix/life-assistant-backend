a268aeed5f659951c415805fef0dc708983ce339
cactusnix <cocalemontea@gmail.com>
2021-03-10 17:04:06 +0800

    one improvement 1.0.1

--- jsbox_part/ONE/scripts/app.js
+++ jsbox_part/ONE/scripts/app.js
@@ -1,27 +1,54 @@
-exports.init = async () => {
+function requestFailed(resp) {
+  return (
+    resp == null || resp.response == null || resp.response.statusCode != 200
+  )
+}
+
+async function fetch() {
   const oneURL = 'http://m.wufazhuce.com/one'
-  const logURL = 'http://image.wufazhuce.com/icon_4.0.png'
   let resp = await $http.get(oneURL)
+  if (requestFailed(resp)) {
+    return {
+      content: '等待时间线刷新...',
+      text_authors: '等待时间线刷新...',
+      img_url: 'http://image.wufazhuce.com/icon_4.0.png',
+      picture_author: 'cactusnix'
+    }
+  }
   let token = resp.data
     .match(/One.token = '.*'/)[0]
     .split(' = ')[1]
     .replaceAll("'", '')
   const apiURL = 'http://m.wufazhuce.com/one/ajaxlist/0?_token=' + token
-  let todayData = (await $http.get(apiURL)).data.data[0]
-  let logoView = {
-    type: 'image',
-    props: {
-      frame: {
-        width: 20,
-        height: 20
-      },
-      position: $point(25, 25),
-      image: $image(logURL),
-      resizable: true
+  let todayResp = await $http.get(apiURL)
+  if (requestFailed(todayResp)) {
+    return {
+      content: '等待时间线刷新...',
+      text_authors: '等待时间线刷新...',
+      img_url: 'http://image.wufazhuce.com/icon_4.0.png',
+      picture_author: 'cactusnix'
     }
   }
+  return todayResp.data.data[0]
+}
+
+exports.init = async () => {
+  let todayData = await fetch()
   $widget.setTimeline({
     render: ctx => {
+      const logURL = 'http://image.wufazhuce.com/icon_4.0.png'
+      let logoView = {
+        type: 'image',
+        props: {
+          frame: {
+            width: 20,
+            height: 20
+          },
+          position: $point(25, 25),
+          image: $image(logURL),
+          resizable: true
+        }
+      }
       switch (ctx.family) {
         case 0:
           return {
